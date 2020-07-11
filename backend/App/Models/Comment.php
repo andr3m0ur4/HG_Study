ecb4fabda636e66df9ec80de21f668551e9aa7c9
André Moura <andre.benedicto@etec.sp.gov.br>
2020-07-11 19:53:53 -0300

    Finalização dos comentários

--- backend/App/Models/Comment.php
+++ backend/App/Models/Comment.php
@@ -6,24 +6,35 @@
 
     class Comment extends Model
     {
+        public function __get($attr)
+        {
+            return $attr;
+        }
+
+        public function __set($attr, $value)
+        {
+            $this->$attr = $value;
+        }
+
         public static function get($id)
         {
             $query = '
                 SELECT
                     c.*,
                     u.name,
-                    u.last_name
+                    u.last_name,
                     u.picture
                 FROM comments AS c
                 INNER JOIN users AS u
                 ON(c.id_user = u.id)
-                WHERE id_post = :id_post';
+                WHERE id_post = :id_post AND id_comment IS NULL
+            ';
 
             $stmt = PARENT::$db->prepare($query);
             $stmt->bindValue(':id_post', $id);
             $stmt->execute();
 
-            return $stmt->fetchAll(\PDO::FETCH_OBJ);
+            return $stmt->fetchAll(\PDO::FETCH_CLASS, __CLASS__);
         }
 
         public static function find($id)
@@ -37,33 +48,83 @@
                 FROM comments AS c
                 INNER JOIN users AS u
                 ON(c.id_user = u.id)
-                WHERE c.id = :id AND c.id_post = :id_post
+                WHERE c.id = :id
             ';
 
             $stmt = PARENT::$db->prepare($query);
             $stmt->bindValue(':id', $id);
-            $stmt->bindValue(':id_post', $id_post);
             $stmt->execute();
 
             return $stmt->fetchObject(__CLASS__);
         }
 
-        public static function create($data)
+        public function getComments()
         {
             $query = '
-                INSERT INTO comments (message, content, quote, content2, picture, id_user)
-                VALUES (:title, :content, :quote, :content2, :picture, :id_user)
+                SELECT
+                    c.*,
+                    u.name,
+                    u.last_name,
+                    u.picture
+                FROM comments AS c
+                INNER JOIN users AS u
+                ON(c.id_user = u.id)
+                WHERE id_comment = :id_comment
             ';
 
             $stmt = PARENT::$db->prepare($query);
-            $stmt->bindValue(':title', $data->title);
-            $stmt->bindValue(':content', $data->content);
-            $stmt->bindValue(':quote', $data->quote);
-            $stmt->bindValue(':content2', $data->content2);
-            $stmt->bindValue(':picture', $data->picture);
+            $stmt->bindValue(':id_comment', $this->id);
+            $stmt->execute();
+
+            return $stmt->fetchAll(\PDO::FETCH_CLASS, __CLASS__);
+        }
+
+        public static function create($data)
+        {
+            $query = '
+                INSERT INTO comments (message, id_post, id_user, id_comment)
+                VALUES (:message, :id_post, :id_user, :id_comment)
+            ';
+
+            $stmt = PARENT::$db->prepare($query);
+            $stmt->bindValue(':message', $data->message);
+            $stmt->bindValue(':id_post', $data->id_post);
             $stmt->bindValue(':id_user', $data->id_user);
+            $stmt->bindValue(':id_comment', $data->id_comment);
             $stmt->execute();
 
             return SELF::find(PARENT::$db->lastInsertId());
+        }
+
+        public function update($data)
+        {
+            foreach ($data as $key => $value) {
+                $this->__set($key, $value);
+            }
+
+            $query = '
+                UPDATE comments
+                SET
+                    message = :message
+                WHERE id = :id
+            ';
+
+            $stmt  =PARENT::$db->prepare($query);
+            $stmt->bindValue(':message', $this->message);
+            $stmt->bindValue(':id', $this->id);
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        public function delete()
+        {
+            $query = 'DELETE FROM comments WHERE id = :id';
+
+            $stmt = PARENT::$db->prepare($query);
+            $stmt->bindValue(':id', $this->id);
+            $stmt->execute();
+
+            return $stmt;
         }
     }
--- backend/src/routes/comments.php
+++ backend/src/routes/comments.php
@@ -8,8 +8,39 @@ use App\Models\Comment;
 
         $this->get('/comments/list/{id}', function(Request $request, Response $response, $args) {
 
-            $comments = Comment::get($args['id']);
+            $comments = [];
+
+            foreach (Comment::get($args['id']) as $comment) {
+                $comment->__set('comments', $comment->getComments());
+                array_push($comments, $comment);
+            }
+
             return $response->withJson($comments);
+
+        });
+
+        $this->post('/comments/add', function(Request $request, Response $response) {
+
+            $data = (object) $request->getParsedBody();
+            $comment = Comment::create($data);
+            return $response->withJson($comment);
+
+        });
+
+        $this->put('/comments/update/{id}', function(Request $request, Response $response, $args) {
+
+            $data = (object) $request->getParsedBody();
+            $comment = Comment::find($args['id']);
+            $comment->update($data);
+            return $response->withJson($comment);
+
+        });
+
+        $this->delete('/comments/delete/{id}', function(Request $request, Response $response, $args) {
+
+			$comment = Comment::find($args['id']);
+			$comment->delete();
+			return $response->withJson($comment);
 
         });
     });
