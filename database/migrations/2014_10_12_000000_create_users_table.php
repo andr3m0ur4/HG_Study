67753c7de0bc3149bd205d0ee6ccd9633550533a
André Moura <andre.benedicto@etec.sp.gov.br>
2023-06-16 07:28:53 -0300

    HG-3 - MER do banco de dados inicial

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('email', 150)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 100);
+            $table->enum('role', ['Admin', 'Leader', 'Apprentice']);
+            $table->string('job', 100);
+            $table->text('bio')->nullable();
+            $table->string('city', 100);
+            $table->string('state', 2);
+            $table->string('current_job', 100)->nullable();
+            $table->text('description')->nullable();
             $table->rememberToken();
+            $table->boolean('remember')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
